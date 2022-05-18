0ac23bbd7be9f6e053998f6bc21ef8d1c070e37f
usameyuce <avusameyuce@outlook.com>
2022-05-18 12:47:15 +0200

    all blog commit

---
+++ src/home.js
@@ -0,0 +1,20 @@
+import BlogList from "./bloglist";
+import useFetch from "./useFetch";
+import React from "react";
+
+const Home = () => {
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
